427111f2300ca6b41d3f061810b934188b855b54
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-01-15 22:42:30 +0700

    merombak file dan menambah file kontak untuk aplikasi utama

---
+++ aplikasi_kontak/lib/kontak.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Kontak extends StatefulWidget {
+  const Kontak({super.key});
+
+  @override
+  State<Kontak> createState() {
+    return _KontakState();
+  }
+}
+
+class _KontakState extends State<Kontak> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          
+        ),
+      ),
+    );
+  }
+}
--- aplikasi_kontak/lib/main.dart
+++ aplikasi_kontak/lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:aplikasi_kontak/start_screen.dart';
+import 'package:aplikasi_kontak/kontak.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    ),
-  );
+  runApp(const Kontak());
 }
