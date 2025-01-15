a8a212df22efe39d641daa20c6e840069a91c4ee
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-01-15 23:30:21 +0700

    menambah warna background sementara  untuk aplikasi

--- aplikasi_kontak/lib/kontak.dart
+++ aplikasi_kontak/lib/kontak.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_kontak/start_screen.dart';
 import 'package:flutter/material.dart';
 
 class Kontak extends StatefulWidget {
@@ -12,10 +13,22 @@ class Kontak extends StatefulWidget {
 class _KontakState extends State<Kontak> {
   @override
   Widget build(BuildContext context) {
+    const Widget screenWidget = StartScreen();
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 255, 138, 177),
+                Color.fromARGB(255, 255, 158, 190),
+              ],
+            ),
+          ),
+          child: screenWidget,
         ),
       ),
     );
