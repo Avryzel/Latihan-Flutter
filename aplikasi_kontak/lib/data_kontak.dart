ce9fde979f9d270c8f08ca52529e4e40eae180bf
Avryzel <128247566+Avryzel@users.noreply.github.com>
2025-02-11 21:45:09 +0700

    membuat file data_kontak untuk menyimpan data nama dan nomor kontak

---
+++ aplikasi_kontak/lib/data_kontak.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DataKontak {
+  DataKontak({
+    required this.nama,
+    required this.nomor,
+  });
+
+  final String nama;
+  final String nomor;
+}
